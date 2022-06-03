6c524fa6844c4c53e0237d897ccb8c75a43533e0
nashrulmazlan7 <84316312+nashrulmazlan7@users.noreply.github.com>
2022-06-04 05:06:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = "2 March 2023";
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds =(newYearsDate - currentDate) /1000;
+    
+    const days = Math.floor(totalSeconds / 3600 / 24)
+    const hours = Math.floor(totalSeconds /3600) % 24;
+    const mins = Math.floor(totalSeconds /60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+
+function formatTime(time){
+    return time < 10 ? (`0${time}`): time;
+
+}
+
+
+const daysEl1 = document.getElementById('days1');
+const hoursEl1 = document.getElementById('hours1');
+const minsEl1 = document.getElementById('mins1');
+const secondsEl1 = document.getElementById('seconds1');
+const newAge = "16 Jan 2023";
+
+function countdown1() {
+    const newAgeDate = new Date(newAge);
+    const currentDate = new Date();
+
+    const totalSeconds =(newAgeDate - currentDate) /1000;
+    
+    const days1 = Math.floor(totalSeconds / 3600 / 24)
+    const hours1 = Math.floor(totalSeconds /3600) % 24;
+    const mins1 = Math.floor(totalSeconds /60) % 60;
+    const seconds1 = Math.floor(totalSeconds) % 60;
+
+    daysEl1.innerHTML = formatTime(days1);
+    hoursEl1.innerHTML = formatTime(hours1);
+    minsEl1.innerHTML = formatTime(mins1);
+    secondsEl1.innerHTML = formatTime(seconds1);
+}
+
+
+
+
+countdown1();
+countdown();
+
+
+setInterval(countdown,1000);
